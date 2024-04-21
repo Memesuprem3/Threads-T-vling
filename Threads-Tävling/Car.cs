99f3c1a8e3d69cc2268e11c6ede4305147b905f2
Christian Rapp <christian.rapp@edu.varberg.se>
2024-04-21 16:02:17 +0200

    Add project files.

---
+++ Threads-Tävling/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Threads_Tävling
+{
+    internal class Car
+    {
+        public string Name {  get; set; }
+        public double Speed { get; set; } = 120; // km/h
+        public double distance { get; set; } //km
+
+        public Car(string Name) 
+        {
+           Name = Name;
+        }
+
+        public void Go(int length)
+        {
+            double time = length / Speed;
+            int Timeinmilisec = (int)(time * 3600 * 1000);
+            Thread.Sleep(Timeinmilisec);
+            distance += length;
+        }
+
+    }
+}
---
+++ Threads-Tävling/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Threads_Tävling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
